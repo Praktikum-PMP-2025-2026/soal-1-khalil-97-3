b5e24f0ca44888f9b39f23fac5d9e30ed9e414a9
khalil-97 <khalilalfaiz79@gmail.com>
2026-05-12 09:52:31 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -2,38 +2,62 @@
 #include <stdlib.h>
 
 
-void preorder(int i, int n, int array) {
-    if (i >= n)
-     return;
+void preorder(int i, int n, int array[]) {
+    if (i >= n) return;
     printf(" %d", array[i]);
     preorder(2 * i + 1, n, array);
     preorder(2 * i + 2, n, array);
 }
 
+
+void inorder(int i, int n, int array[]) {
+    if (i >= n) return;
+    inorder(2 * i + 1, n, array); 
+    printf(" %d", array[i]);
+    inorder(2 * i + 2, n, array);  
+}
+
+
+void postorder(int i, int n, int array[]) {
+    if (i >= n) return;
+    postorder(2 * i + 1, n, array); 
+    postorder(2 * i + 2, n, array); 
+    printf(" %d", array[i]);
+}
+
 int main() {
     int n;
     
-
+    
     if (scanf("%d", &n) != 1) {
         return 0;
     }
-
-
-    int *array (int*)malloc(n * sizeof(int));
-   
     
-   
+  
+    int *array = (int*)malloc(n * sizeof(int));
+    
+ 
     for (int i = 0; i < n; i++) {
         scanf("%d", &array[i]);
     }
 
-    
+
     printf("PRE");
     preorder(0, n, array);
     printf("\n");
 
+ 
+    printf("IN");
+    inorder(0, n, array);
+    printf("\n");
 
-    free(array)
+ 
+    printf("POST");
+    postorder(0, n, array);
+    printf("\n");
+
+   
+    free(array);
     
     return 0;
 }
