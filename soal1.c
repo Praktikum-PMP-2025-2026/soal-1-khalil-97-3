e3b59d44d34472bb58d69f14dbc7f14e05b34dc4
khalil-97 <khalilalfaiz79@gmail.com>
2026-05-12 09:20:42 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+
+void preorder(int i, int n, int array) {
+    if (i >= n)
+     return;
+    printf(" %d", array[i]);
+    preorder(2 * i + 1, n, array);
+    preorder(2 * i + 2, n, array);
+}
+
+int main() {
+    int n;
+    
+
+    if (scanf("%d", &n) != 1) {
+        return 0;
+    }
+
+
+    int *array (int*)malloc(n * sizeof(int));
+   
+    
+   
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    
+    printf("PRE");
+    preorder(0, n, array);
+    printf("\n");
+
+
+    free(array)
+    
     return 0;
 }
